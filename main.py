3250205c7158d6d6922cfa49afeb3701d24c8635
AdvaitThePro <66001714+AdvaitThePro@users.noreply.github.com>
2022-02-02 08:47:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/profile")
+def profile():
+    return "<h1>Profile Page</h1>"
+
+@app.route("/search")
+def search():
+    return "<h1>Seach Page</h1>"
+
+@app.route("/follow")
+def follow():
+    return "<h1>Follow Page</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5000")
